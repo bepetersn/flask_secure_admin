44e2dabfc06cd928db1405407fa635e2b117643b
Brian Peterson <brian.e.peterson@colorado.edu>
2019-08-13 15:15:10 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/me/myproject'
 EMAIL = 'bepetersondev@gmail.com'
 AUTHOR = 'Brian Peterson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.17'
+VERSION = '0.1.18'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
